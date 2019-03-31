c37307cd8013b369dcd150ca2c53accf2c841c79
Arianna Choza <unachoza@gmail.com>
2019-03-31 14:01:51 -0400

    wired up routing for stream pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,30 +1,21 @@
 import React from 'react'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
+import StreamCreate from './streams/StreamCreate'
+import StreamEdit from './streams/StreamEdit'
+import StreamDelete from './streams/StreamDelete'
+import StreamList from './streams/StreamList'
+import StreamShow from './streams/StreamShow'
 
-const PageOne = () => {
-    return (
-        <div>
-            PageOne<br/>
-            <Link to="/pagetwo"><button>Back</button></Link>
-        </div>
-    )
-}
-const PageTwo = () => {
-    return (
-        <div>
-            PageTwo<br/>
-            <Link to="/"><button>Back</button></Link>
-        </div>
-    )
-    
-}
 const App = () => {
     return (
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/"  exact component={PageOne} />
-                    <Route path="/pagetwo" component={PageTwo} />
+                    <Route path="/"  exact component={StreamList} />
+                    <Route path="/streams/new"  exact component={StreamCreate} />
+                    <Route path="/streams/edit"  exact component={StreamEdit} />
+                    <Route path="/streams/delete"  exact component={StreamDelete} />
+                    <Route path="/streams/show"  exact component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
