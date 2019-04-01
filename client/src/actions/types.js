7ec9daa0f19813c4994a24c773a57b3697d2e5ad
Arianna Choza <unachoza@gmail.com>
2019-04-01 17:57:02 -0400

    REST CRUD Action Creators

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,7 @@
-export const SIGN_IN = "SIGN_IN"
 export const SIGN_OUT = "SIGN_OUT"
+export const SIGN_IN = "SIGN_IN"
+export const CREATE_STREAM = "CREATE_STREAM"
+export const FETCH_STREAMS = "FETCH_STREAMS"
+export const FETCH_STREAM = "FETCH_STREAM"
+export const DELELTE_STREAM = "DELELTE_STREAM"
+export const EDIT_STREAM = "EDIT_STREAM"
