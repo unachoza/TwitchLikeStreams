6dd8fb827145157aa230694b00bcf218be9d483b
Arianna Choza <unachoza@gmail.com>
2019-04-01 00:20:47 -0400

    google id is now typed to user who is logged in

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -19,7 +19,7 @@ class GoogleAuth extends Component{
     }
 
     onAuthChange = (isSignedIn) => {
-        isSignedIn ? this.props.signIn() : this.props.signOut()
+        isSignedIn ? this.props.signIn(this.auth.currentUser.get().getId()) : this.props.signOut()
        
     }
 
