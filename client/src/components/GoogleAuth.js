8cd143b71054176304a0f4cf132b967313ef5949
Arianna Choza <unachoza@gmail.com>
2019-03-31 23:24:31 -0400

    google auth renders signed in or out status

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react'
 
 class GoogleAuth extends Component{
+    state = {
+        isSignedIn: null
+    }
 
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId : '820118529267-k0spahsabie4dqajgnhplqb82ao57vsh.apps.googleusercontent.com',
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance()
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+
             })
         })
     }
+
+    renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+        return <div>Maybe</div>
+    } else if (this.state.isSignedIn) {
+        return <div>Arianna is Signed In!</div>
+    } else {
+        return <div>No one is signed in.</div>
+    }
+    
+    }
     render() {
         return (
-            <div>GoogleAuth</div>
+            <div>{this.renderAuthButton()}</div>
         )
     }
 }
