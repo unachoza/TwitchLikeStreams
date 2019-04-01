18b4b24b69b76d62fe4639d7a4fdb237b4a349c4
Arianna Choza <unachoza@gmail.com>
2019-04-01 09:49:24 -0400

    created validate helper function to show/prevent users empty form errors

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -5,21 +5,37 @@ class StreamCreate extends Component {
     renderInput({input, label}) {
         return (
             <div className="field">
-                <lable>{label}</lable>
+                <label>{label}</label>
                 <input {...input} />
             </div>
             
         )
     }
+
+    onSubmit(formValues) {
+        
+    }
+
     render() {
         return (
-            <form className="ui form">
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
                 <Field name='title' component={this.renderInput} label="Enter Title" />
-                <Field name='description' component={this.renderInput} label="Enter Description"/>
+                <Field name='description' component={this.renderInput} label="Enter Description" />
+                <button className="ui button primary">Submit</button>
             </form>
         )
     }
 }
+   const validate = formValues => {
+        const errors = {}
+        if (!formValues.title) {
+            errors.title = "You must enter a title"
+        }
+        if (!formValues.description) {
+            errors.description = "You must enter a description"
+        }
+        return errors  
+    }
 
 export default reduxForm({
     form: 'streamCreate'
