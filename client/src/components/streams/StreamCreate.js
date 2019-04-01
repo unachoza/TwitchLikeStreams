349dfb6bcd51e7d95667d51251486957a1ff3ae8
Arianna Choza <unachoza@gmail.com>
2019-04-01 10:01:37 -0400

    error messages displayed for user

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react'
 import {Field, reduxForm } from 'redux-form'
 
 class StreamCreate extends Component {
-    renderInput({input, label}) {
+    renderInput = ({ input, label, meta }) =>{
+        console.log(meta)
         return (
             <div className="field">
                 <label>{label}</label>
-                <input {...input} />
+                <input {...input} autoComplete="off" />
+                {this.renderError(meta)}
             </div>
             
         )
+    } 
+    renderError({ error, touched }) {
+        if (touched && error) {
+            return <div className="ui error message">
+                <div className="header">{error}</div>
+            </div>
+        }
     }
+
+   
 
     onSubmit(formValues) {
         
@@ -18,7 +29,7 @@ class StreamCreate extends Component {
 
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
                 <Field name='title' component={this.renderInput} label="Enter Title" />
                 <Field name='description' component={this.renderInput} label="Enter Description" />
                 <button className="ui button primary">Submit</button>
@@ -38,5 +49,6 @@ class StreamCreate extends Component {
     }
 
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate
 })(StreamCreate)
