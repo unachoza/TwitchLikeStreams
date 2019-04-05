2979e7353c3be4a5e3f3e7ed44450704382ed520
Arianna Choza <unachoza@gmail.com>
2019-04-05 18:44:18 -0400

    building out edit stream. fetching single data entry

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import {fetchStream} from '../../actions/index'
 
-const StreamEdit = () => {
-    return <div>StreamEdit</div>
+class StreamEdit extends Component{
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id)
+    }
+
+    render() {
+        if (!this.props.stream) {
+            return(<div>loading</div>)
+        }
+        console.log(this.props)
+        return (
+            <div>
+                {this.props.stream.title}
+            </div>
+        )
+      
+  }
 }
-
-export default StreamEdit
+const mapStateToProps = (state, ownProps) => {
+  
+    return {stream: state.streams[ownProps.match.params.id]}
+}
+export default connect(mapStateToProps, {fetchStream})(StreamEdit)
--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -11,13 +11,11 @@ class StreamList extends Component{
     }
 
     renderAdimPermissions = (stream) => {
-        console.log(stream.userId, this.props.currentUserId)
+        // console.log(stream.userId, this.props.currentUserId)
         if (stream.userId === this.props.currentUserId) {
             return (
                 <div className="right floated content">
-                    <button className="ui button primary">
-                        Edit
-                    </button>
+                    <Link to={`streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
                     <button className="ui button negative">
                         Delete
                     </button>
